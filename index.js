f122454c30491cadad38c65ff04e03bb73bf4bed
John <jrc2855@gmail.com>
2023-03-06 21:51:58 +0000

    Scaffolding, and Essentials

---
+++ index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3002;
+
+app.get('/', (req, res, next) => {
+  res.status(200).send('Proof of life from our base endpoint.');
+});
+
+app.get('/forwardslash', (req, res, next) => {
+  res.status(200).send('This is a redundantly confusing endpoint.');
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
